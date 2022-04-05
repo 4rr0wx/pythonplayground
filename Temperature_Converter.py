f60522c6f8a159318eeb16a37b2d6c31fa5e1253
4rr0wx <35532725+4rr0wx@users.noreply.github.com>
2022-04-05 07:32:20 +0200

    add erstabfrage with unit chooser

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,2 +1,4 @@
 ## Temperature Converter
-print("Tempconverter V0.1")
+print("Tempconverter V0.1 /n")
+print("Mit diesem Programm kann man jede Temperatureinheit in jede andere Temperatureinheit umwandeln /n")
+print("Bitte Starteinheit auswählen: /n C: Celsius /n F: Fahrenheit /n K:Kelvin")
