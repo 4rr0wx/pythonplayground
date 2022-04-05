e0e2184c840ef0e4cf26ca3f43f88e89912092d2
4rr0wx <35532725+4rr0wx@users.noreply.github.com>
2022-04-05 08:36:29 +0200

    add selection unit1 and input temp1

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,4 +1,24 @@
 ## Temperature Converter
-print("Tempconverter V0.1 /n")
-print("Mit diesem Programm kann man jede Temperatureinheit in jede andere Temperatureinheit umwandeln /n")
-print("Bitte Starteinheit auswählen: /n C: Celsius /n F: Fahrenheit /n K:Kelvin")
+print("Tempconverter V0.1 \n")
+print("Mit diesem Programm kann man jede Temperatureinheit in jede andere Temperatureinheit umwandeln \n")
+print("Bitte Starteinheit auswählen: \n 1: Celsius \n 2: Fahrenheit \n 3:Kelvin")
+
+einheit1 = input("\n Einheit:")
+print("Gewählte Einheit:", einheit1 )
+
+einheit1 = float(einheit1)
+
+while einheit1 < 0 or einheit1 > 3:
+    print("Falsche Auswahl! \n Bitte Wiederholen")
+    einheit1 = input("\n Einheit:")
+    print("Gewählte Einheit:", einheit1)
+    einheit1 = float(einheit1)
+
+temp1 = input("Bitte erste Temperatur in der gewählten Einheit eingeben:\n")
+print("Eingegebene Tamperatur:", temp1)
+
+if einheit1 = 1:
+
+elif einheit1 = 2:
+
+elif einheit1 = 3:
